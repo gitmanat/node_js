68b66081d27fb106933d347c03f9ae1950c0d5a3
ManatHome <manatccna@gmail.com>
2016-04-09 12:21:23 +0700

    JS : Closure

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-// JavaScript : First-class function
-// 3. function return function
+// JavaScript : Closure
+// Closure = function all env parent
 
-function say() {
-	return function myName() {
-		console.log("Stis & Photon");
+function parent() {
+	var x = 1;
+
+	function child() {
+		console.log(x);
 	}
+	return child;
 }
-
-var nameFn = say();
-nameFn();
+var childFn = parent();
+childFn();
