a4b289e9c1e9a59932825f4b2c597adf74bf0f2f
ManatHome <manatccna@gmail.com>
2016-04-09 15:00:06 +0700

    Connect module : Order middleware

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-// Connect module : Connect middleware
+// Connect module : Order middleware
 
 var connect = require('connect');
 var app = connect();
 
-app.use(function(req, res, next) {
-	//middleware
+var logger = function(req, res, next) {
+	console.log(req.method, req.url);
+
+	next();
+}
+
+var hello = function(req, res, next) {
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Hello Stis & Photon');
-});
+};
+app.use(logger);
+app.use(hello);
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000');
