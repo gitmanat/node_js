76294da4092b4eb6afd5f40287c8d45f661ee6e0
ManatHome <manatccna@gmail.com>
2016-04-09 14:52:22 +0700

    Connect module : Connect middleware

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-// Connect module : management request by Connect module
+// Connect module : Connect middleware
 
 var connect = require('connect');
 var app = connect();
 
-app.listen(3000);
+app.use(function(req, res, next) {
+	//middleware
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello Stis & Photon');
+});
 
+app.listen(3000);
 console.log('Server running at http://localhost:3000');
