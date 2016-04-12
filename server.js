79efb91c141e755dc27873b825fd0387a0e572b0
ManatOffice <manatccna@gmail.com>
2016-04-12 14:02:53 +0700

    MVC : web app

--- server.js
+++ server.js
@@ -1,25 +1,5 @@
-// Express module : express wrapper
-
-var express = require('express');
+var express = require('./config/express');
 var app = express();
-
-var logger = function(req, res, next) {
-	console.log(req.method, req.url);
-
-	next();
-}
-
-var hello = function(req, res, next) {
-	res.send('Hello Stis & Photon');       //Express wrapper : Setting Content-Type same argument
-};
-
-var goodbye = function(req, res, next) {
-	res.send('Goodbye Stis & Photon');     //Express wrapper : Setting Content-Type same argument
-};
-
-app.use(logger);
-app.use('/hello', hello);
-app.use('/goodbye', goodbye);
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000');
