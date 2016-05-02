a763759251496328f49299ebaaa84ff1dea6fe16
ManatOffice <manatccna@gmail.com>
2016-05-02 09:47:24 +0700

    static file : express.static()

--- config/express.js
+++ config/express.js
@@ -20,5 +20,8 @@ module.exports = function() {
     app.set('view engine', 'jade');
 
     require('../app/routes/index.routes')(app);
+
+    app.use(express.static('./public'));
+    
     return app;
 };
