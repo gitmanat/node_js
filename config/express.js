b8b06a7a0ead95fb5787dd96d4bce0f67f886607
ManatOffice <manatccna@gmail.com>
2016-05-04 21:49:12 +0700

    form : input show logs

--- config/express.js
+++ config/express.js
@@ -21,6 +21,7 @@ module.exports = function() {
     app.set('view engine', 'jade');
 
     require('../app/routes/index.routes')(app);
+    require('../app/routes/user.routes')(app);
 
     app.use(sass({
         src: './sass',
