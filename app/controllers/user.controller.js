c5f85f4e8a0d75c682fb8f6f0912af9798447ca6
ManatOffice <manatccna@gmail.com>
2016-05-20 12:07:40 +0700

    CRUD: Read document(find)

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -12,6 +12,16 @@ exports.create = function(req, res, next) {
 	}) ;
 };
 
+exports.list = function(req, res, next) {
+	User.find({}, function(err, users) {
+		if (err) {
+			return next(err);
+		} else {
+			res.json(users);
+		}
+	});
+};
+
 //*************************************
 
 exports.login = function(req, res) {
