a3fbc3fc6ca6e7331f55f89ecf212b5ca18ca03c
ManatOffice <manatccna@gmail.com>
2016-05-20 15:44:21 +0700

    CRUD: Delete document(delete)

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -38,6 +38,16 @@ exports.update = function(req, res, next) {
         });
 };
 
+exports.delete = function(req, res, next) {
+    req.user.remove(function(err) {
+        if (err) {
+            return next(err);
+        } else {
+            res.json(req.user);
+        }
+    });
+};
+
 exports.userByUsername = function(req, res, next,username) {
     User.findOne({
         username: username
