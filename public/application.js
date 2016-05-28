3bebb445756a9e6750511bb6b3d882646e9fbd79
ManatOffice <manatccna@gmail.com>
2016-05-28 07:57:51 +0700

    2-way data binding

--- public/application.js
+++ public/application.js
@@ -2,6 +2,10 @@
 
 var mainAppModule = angular.module('Hello', []);
 
+mainAppModule.controller('NameController', ['$scope', function($scope) {
+	$scope.yourName = 'No Name';
+}]);
+
 mainAppModule.filter('sayhello', function() {
 	return function(name) {
 		return 'Hello, ' + name;
