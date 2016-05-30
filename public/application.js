ee23f7ef24f1632dde77d8f42c3205d219ca822a
ManatOffice <manatccna@gmail.com>
2016-05-31 06:09:55 +0700

    Routing by UI-Router

--- public/application.js
+++ public/application.js
@@ -3,18 +3,10 @@
 var mainAppModuleName = 'Main';
 var mainAppModule = angular.module(mainAppModuleName, ['ui.router','hello']);
 
-mainAppModule.controller('NameController', ['$scope', function($scope) {
-	$scope.yourName = 'No Name';
-}]);
-
-mainAppModule.filter('sayhello', function() {
-	return function(name) {
-		return 'Hello, ' + name;
-	};
-});
-
 angular.element(document).ready(function() {
 	angular.bootstrap(document.querySelector('#mainApp'), [mainAppModuleName], {
 		strictDi: true
 	});
 });
+
+
