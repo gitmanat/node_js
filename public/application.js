04c8f99d1755f89e449f9e3604fd0d2fbf34613f
ManatOffice <manatccna@gmail.com>
2016-06-15 19:57:43 +0700

    Sign up/Log in Form by Angular

--- public/application.js
+++ public/application.js
@@ -2,7 +2,7 @@
 
 var mainAppModuleName = 'Main';
 var mainAppModule = angular.module(mainAppModuleName,
-    ['ui.router', 'core', 'hello']);
+    ['ui.router', 'core', 'users']);
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document.querySelector('#mainApp'), [mainAppModuleName], {
--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -6,3 +6,5 @@ angular.module('core').config([
 		$urlRouterProvider.otherwise('/');
 	}
 ]);
+
+
